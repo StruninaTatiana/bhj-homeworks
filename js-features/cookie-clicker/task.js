91d508bb1258fbf6e97ae9a76747010e89657950
StruninaTatiana <101055450+StruninaTatiana@users.noreply.github.com>
2022-07-05 17:30:52 +0300

    Correction

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -4,10 +4,10 @@ let cookieClick = document.getElementById('cookie');
 cookieClick.onclick = function() {
   outputClick.textContent = Number(outputClick.textContent) + 1;
 
-  if (outputClick.textContent % 2 === 0) {
-    cookieClick.width = 200;
-  } else {
+  if (cookieClick.width === 200) {
     cookieClick.width = 250;
+  } else {
+    cookieClick.width = 200;
   };
 };
 
