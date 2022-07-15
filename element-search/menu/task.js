4be2876e0a57e1036a479d396172be9f7f4a495f
StruninaTatiana <101055450+StruninaTatiana@users.noreply.github.com>
2022-07-15 12:03:27 +0300

    Correction menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -5,18 +5,20 @@ for(let i = 0; i < menuItems.length - 1; i++) {
   if (menuItems[i].closest('.menu_sub') === null && menuItems[i + 1].closest('.menu_sub') !== null) {
 
     menuItems[i].onclick = function() {
-
+    
       if (document.querySelector('.menu_active') !== null) {
+
+        if (menuItems[i + 1].closest('.menu_sub').className.includes('menu_active') ) {
+          menuItems[i + 1].closest('.menu_sub').classList.remove('menu_active');
+          return;   
+        };
+
         document.querySelector('.menu_active').classList.remove('menu_active')
       };
 
       menuItems[i + 1].closest('.menu_sub').classList.add('menu_active'); 
       return false;
-    } /* else {
-      menuItems[i].onclick = function() {
-       // события по клику
-      };
-    }*/;
+    };
   };
 };
 
