820b15bbb534619a9bfd9bf91811ce0077387b69
StruninaTatiana <101055450+StruninaTatiana@users.noreply.github.com>
2022-07-15 12:07:20 +0300

    Correction slider

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,24 +2,16 @@ const arrowPrev = document.querySelector('.slider__arrow_prev');
 const arrowNext = document.querySelector('.slider__arrow_next');
 const sliderItems = Array.from(document.querySelectorAll('.slider__item'));
 
-let i = 0;
-
 arrowPrev.onclick = function() {
-  sliderItems[i].classList.remove('slider__item_active');    
-  if (i === 0) {
-    i = 4;
-  } else {
-    i--;
-  }
-  sliderItems[i].classList.add('slider__item_active');
+  let index = sliderItems.findIndex((elem) => elem.className.includes('slider__item_active'));
+  sliderItems[index].classList.remove('slider__item_active'); 
+  index === 0 ? index = sliderItems.length - 1 : index--;
+  sliderItems[index].classList.add('slider__item_active');
 };
 
 arrowNext.onclick = function() {
-  sliderItems[i].classList.remove('slider__item_active');
-  if (i === 4) {
-    i = 0;
-  } else {
-    i++;
-  }
-  sliderItems[i].classList.add('slider__item_active');
-};    
+  let index = sliderItems.findIndex((elem) => elem.className.includes('slider__item_active'));
+  sliderItems[index].classList.remove('slider__item_active'); 
+  index === sliderItems.length - 1 ? index =  0 : index++;
+  sliderItems[index].classList.add('slider__item_active');
+};
