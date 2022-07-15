d1aaa19b5483ef3eb6b241304911e21220cc6f42
StruninaTatiana <101055450+StruninaTatiana@users.noreply.github.com>
2022-07-15 12:05:38 +0300

    Correction popups

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,19 +1,21 @@
 document.getElementById('modal_main').classList.add('modal_active');
 
-let elementsClose = Array.from(document.getElementsByClassName('modal__close'));
+const elementsClose = Array.from(document.getElementsByClassName('modal__close'));
 
-for(let i = 0; i < elementsClose.length; i++) {
-  elementsClose[i].onclick = function() {
+for(let element of elementsClose) {
+  element.onclick = function() {
+    this.closest('.modal').classList.remove('modal_active');
     document.getElementById('modal_main').classList.remove('modal_active');
-    document.getElementById('modal_success').classList.remove('modal_active');
-
   };
 };
 
-let elementsSuccess = Array.from(document.getElementsByClassName('show-success'));
+const elementsSuccess = Array.from(document.getElementsByClassName('show-success'));
 
-for(let i = 0; i < elementsSuccess.length; i++) {
-  elementsSuccess[i].onclick = function() {
+for(let element of  elementsSuccess) {
+  element.onclick = function() {
+    this.closest('.modal').classList.remove('modal_active');
     document.getElementById('modal_success').classList.add('modal_active');
   };
 };
+
+
